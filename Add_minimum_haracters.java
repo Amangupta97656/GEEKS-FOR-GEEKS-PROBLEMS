e201595bc0dd380dff21803dc855bc73daa05687
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2023-04-07 18:14:14 +0530

    Add files via upload

---
+++ Add_minimum_haracters.java
@@ -0,0 +1,34 @@
+class Solution
+{
+	public static int addMinChar(String str){
+	 int i;
+		    int j = 1; 
+		    int maxLength = 0;
+		    int copyJ = j;
+		    while(j<str.length()){
+	            i=0;
+	            copyJ = j;
+		        while(i<j){
+		            if(str.charAt(i)!=str.charAt(j)){
+		                break;
+		            }
+		            j--;
+		            i++;
+		        }
+		        if(i>=j){
+		            //Palindrome was found, hence calculate length
+		            maxLength = Math.max(copyJ+1,maxLength);
+		        }
+		        
+		        j = copyJ + 1;
+		    }
+		    
+		    if(maxLength==0){
+		        return str.length()-1;
+		    }
+		    else {
+		        return (str.length()-maxLength);
+		    }
+
+	}
+}
