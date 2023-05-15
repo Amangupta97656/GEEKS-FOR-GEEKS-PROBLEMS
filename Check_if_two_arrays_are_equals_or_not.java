3282cf02841899df2b98257e03e60a186c4b4445
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2023-05-15 20:29:37 +0530

    Add files via upload

---
+++ Check_if_two_arrays_are_equals_or_not.java
@@ -0,0 +1,17 @@
+class Solution{
+    //Function to check if two arrays are equal or not.
+    public static boolean check(long A[],long B[],int N)
+    {
+        //Your code here
+        if(A.length==B.length){
+            Arrays.sort(A);
+            Arrays.sort(B);
+            for(int i=0;i<N;i++){
+                if(A[i]!=B[i]){
+                    return false;
+                   
+                }
+            }
+        }return true;
+    }
+}
