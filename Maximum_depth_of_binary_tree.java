fb37aadb7ca0044c3cbd4912d32d85673da0ea47
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2023-06-28 19:12:56 +0530

    Add files via upload

---
+++ Maximum_depth_of_binary_tree.java
@@ -0,0 +1,11 @@
+class Solution {
+  public static int maxDepth(Node root) {
+    
+    if(root==null ){
+        return 0;
+    }
+    return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+    
+
+  }
+}
