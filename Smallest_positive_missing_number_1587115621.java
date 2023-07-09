8bd8ccffe6b2b56027ef66bc898182408d9632a2
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2023-07-09 22:17:19 +0530

    Add files via upload

---
+++ Smallest_positive_missing_number_1587115621.java
@@ -0,0 +1,21 @@
+class Solution
+{
+    //Function to find the smallest positive number missing from the array.
+    static int missingNumber(int arr[], int size)
+    {
+        // Your code here
+        boolean flag= false;
+        int ans=0;
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<size;i++){
+            set.add(arr[i]);
+        }
+        for(int i=0;i<size;i++){
+            if(set.contains(i+1))continue;
+            else{
+                return i+1;
+            }
+        }
+        return size+1;
+    }
+}
