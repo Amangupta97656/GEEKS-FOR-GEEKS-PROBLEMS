0d93bd6afa7ea00b1588aa298da9874e949f7df5
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2023-07-16 21:05:39 +0530

    Add files via upload

---
+++ Queue_reversal.java
@@ -0,0 +1,14 @@
+class GfG{
+    //Function to reverse the queue.
+    public Queue<Integer> rev(Queue<Integer> q){
+        //add code here.
+        Stack<Integer> s = new Stack<>();
+        while (!q.isEmpty()) {
+            s.push(q.remove());
+        }
+        while (!s.isEmpty()) {
+            q.add(s.pop());
+        }
+        return q;
+    }
+}
