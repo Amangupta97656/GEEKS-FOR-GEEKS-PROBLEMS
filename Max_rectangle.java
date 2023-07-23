6f5838c81d04ab8e401c43b2b92638d78846d12c
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2023-07-23 21:47:16 +0530

    Add files via upload

---
+++ Max_rectangle.java
@@ -0,0 +1,70 @@
+class Solution {
+    private int[] nextSmallerElement(int[] arr, int n) {
+        Stack<Integer> s = new Stack<>();
+        s.push(-1);
+        int[] ans = new int[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            int curr = arr[i];
+            while (!s.isEmpty() && s.peek() != -1 && arr[s.peek()] >= curr) {
+                s.pop();
+            }
+            // ans is stack's top
+            ans[i] = s.peek();
+            s.push(i);
+        }
+        return ans;
+    }
+
+    private int[] prevSmallerElement(int[] arr, int n) {
+        Stack<Integer> s = new Stack<>();
+        s.push(-1);
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int curr = arr[i];
+            while (!s.isEmpty() && s.peek() != -1 && arr[s.peek()] >= curr) {
+                s.pop();
+            }
+            // ans is stack's top
+            ans[i] = s.peek();
+            s.push(i);
+        }
+        return ans;
+    }
+
+    private int largestRectangleArea(int[] heights, int n) {
+        int[] next = nextSmallerElement(heights, n);
+        int[] prev = prevSmallerElement(heights, n);
+
+        int area = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int l = heights[i];
+
+            if (next[i] == -1) {
+                next[i] = n;
+            }
+            int b = next[i] - prev[i] - 1;
+            int newArea = l * b;
+            area = Math.max(area, newArea);
+        }
+        return area;
+    }
+
+    public int maxArea(int[][] M, int n, int m) {
+        int[] heights = M[0];
+        int area = largestRectangleArea(heights, m);
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (M[i][j] != 0) {
+                    M[i][j] += M[i - 1][j];
+                } else {
+                    M[i][j] = 0;
+                }
+            }
+            area = Math.max(area, largestRectangleArea(M[i], m));
+        }
+        return area;
+    }
+}
