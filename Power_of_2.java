b71c00c99f0e8073f970cc5bcc96934f548c3651
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2023-09-18 20:44:36 +0530

    Add files via upload

--- Power_of_2.java
+++ Power_of_2.java
@@ -2,14 +2,15 @@ class Solution{
     
     // Function to check if given number n is a power of two.
     public static boolean isPowerofTwo(long n){
-       long pow=1;
-        while(pow<=n){
-            if(pow==n){
-                return true;
-            }
-            pow*=2;
+        
+        // Your code here
+        if(n==0) return false;
+        if(n==1) return true;
+        while(n>1){
+            if(n%2!=0) return false;
+            n/=2;
         }
-        return false;
+        return true;
     }
     
 }
