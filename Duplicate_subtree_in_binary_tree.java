ed141a946acbd37a68f750929471e3d05039fd0c
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2023-10-12 20:55:54 +0530

    Add files via upload

---
+++ Duplicate_subtree_in_binary_tree.java
@@ -0,0 +1,50 @@
+class Solution {
+    
+    
+    
+    Map<String, Integer> mp = new HashMap<>();
+    
+    String recur (Node root){
+        if(root ==null){
+            return "$";
+        }
+        String s = "";
+        if(root.left==null && root.right==null){
+            s+=root.data;
+            return s;
+        }
+        
+        
+       
+       s+=root.data;
+        s+=recur(root.left);
+        s+=recur(root.right);
+        
+     
+         mp.put(s,mp.getOrDefault(s,0)+1);
+         
+         return s;
+        
+        
+    }
+    int dupSub(Node root) {
+    
+        
+        recur(root);
+        for(Integer num : mp.values()){
+            
+            if(num >=2){
+                return 1;
+            }
+            
+        
+            
+        }
+        
+        return 0;
+        
+        
+        
+    }
+}
+
