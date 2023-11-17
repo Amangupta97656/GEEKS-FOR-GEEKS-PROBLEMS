d804d0006275855b9904015759212dc153ed880d
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2023-11-17 22:34:32 +0530

    Add files via upload

---
+++ Binary_tree_to_cdll.java
@@ -0,0 +1,30 @@
+class Solution
+{ 
+    //Function to convert binary tree into circular doubly linked list.
+    Node bTreeToClist(Node root)
+    {
+        //your code here
+        ArrayList<Node> al = new ArrayList<>();
+        al = inorder(root,al);
+        Node head =al.get(0);
+        Node last=null;
+        for(int i=0;i<al.size()-1;i++){
+            Node n1 =  al.get(i);
+            Node n2 = al.get(i+1);
+            n1.right=n2;
+            n2.left=n1;
+            last=n2;
+        }
+        head.left=last;
+        last.right=head;
+        
+        return head;
+    }
+    ArrayList<Node> inorder(Node root,ArrayList<Node>al){
+        if(root==null)return  al;
+        
+        inorder(root.left,al);
+        al.add(root);
+        return al= inorder(root.right,al);
+    }
+}
