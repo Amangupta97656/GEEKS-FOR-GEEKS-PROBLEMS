aea01ccbe0692e23058b92266312e4d8db55b7b7
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2023-12-31 17:01:16 +0530

    Add files via upload

---
+++ New_year_resolution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static boolean isPossible(int n, int[] coins) {
+        return helper(0,coins,n,0);
+    }
+    public static boolean helper(int idx,int coins[],int n,int sum){
+        if(sum!=0 && (sum%2024==0 ||sum%20==0||sum%24==0)){
+            return true;
+        }
+        if(idx==coins.length){
+            return false;
+        }
+        
+        boolean temp1=helper(idx+1,coins,n,sum+coins[idx]);
+        if(temp1==true)return true;
+        
+        boolean temp2=helper(idx+1,coins,n,sum);
+        if(temp2==true)return true;
+        
+        return false;
+    }
+}
