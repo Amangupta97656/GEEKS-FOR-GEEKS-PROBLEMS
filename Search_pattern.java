6d9d91923d8b5e00cc7f384d19a25936259934ac
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-01-09 20:16:18 +0530

    Add files via upload

---
+++ Search_pattern.java
@@ -0,0 +1,12 @@
+class Solution{
+    
+    ArrayList<Integer> search(String pat, String txt){
+        ArrayList<Integer> list = new ArrayList<>();
+        int index = txt.indexOf(pat);
+        while(index != -1){
+            list.add(index+1);
+            index=txt.indexOf(pat,index+1);
+        }
+        return list;
+    }
+}
