9325eb6b7dbae80204cc8f22aef53a3129d00d57
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-01-10 20:50:35 +0530

    Add files via upload

---
+++ Longest_subarray_with_sum_divisible_by_k.java
@@ -0,0 +1,25 @@
+class Solution{
+    int longSubarrWthSumDivByK(int a[], int n, int k)
+    {
+        //contain reminder and it's position 
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int maxLength = Integer.MIN_VALUE;
+        map.put(0,-1);
+        int sum = 0;
+        int reminderSum=0;
+        for(int i=0;i<n;i++){
+            sum += a[i];
+            reminderSum = sum%k;
+            if(reminderSum<0){
+                reminderSum += k;
+            }
+            if(!map.containsKey(reminderSum)){
+                map.put(reminderSum,i);
+            }else{
+                maxLength = Math.max(maxLength,i-map.get(reminderSum));
+            }
+        }
+        
+        return (maxLength==Integer.MIN_VALUE)?0:maxLength;
+    }
+}
