aac2c035457baf3fc6b09f91d88e0fc6a99294a7
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-01-26 21:52:59 +0530

    Add files via upload

--- Fractional_knapsack.java
+++ Fractional_knapsack.java
@@ -1,33 +1,22 @@
 class Solution
 {
     //Function to get the maximum total value in the knapsack.
-     double fractionalKnapsack(int W, Item arr[], int n)
+    double fractionalKnapsack(int W, Item arr[], int n) 
     {
-        Comparator<Item> comp = new Comparator<Item>() {
-            @Override
-            public int compare(Item o1, Item o2) {
-                double ratio1 = (double)o1.value/o1.weight;
-                double ratio2 = (double)o2.value/o2.weight;
-                if(ratio1<ratio2){
-                    return 1;
-                }else{
-                    return -1;
-                }
-            }
-        };
-        Arrays.sort(arr,comp);
-        double val=0;
-        for(int i=0;i<arr.length;i++){
-            if(W>=arr[i].weight){
-                val+=arr[i].value;
-                W-=arr[i].weight;
-            }else if(W>0){
-                val+=((double)arr[i].value/arr[i].weight)*W;
-                break;
-            }else{
-                break;
-            }
+        // Your code here
+        Arrays.sort(arr,(a,b)->Double.compare(b.value*1.0/b.weight,a.value*1.0/a.weight));//double compare is imp for correct ans
+        
+        double ans = 0;
+        int i=0;
+        while(i<n && arr[i].weight<=W){
+            ans += arr[i].value;
+            W -= arr[i].weight;
+            i++;
         }
-        return val;
+        
+        if(i<n && W>0)
+        ans += (arr[i].value*1.0/arr[i].weight) * W;
+            
+        return ans;
     }
 }
