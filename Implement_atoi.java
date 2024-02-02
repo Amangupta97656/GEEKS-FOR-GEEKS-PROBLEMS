9f5b38b8f437492636019661e7589de2c24eb715
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-02-02 22:22:10 +0530

    Add files via upload

--- Implement_atoi.java
+++ Implement_atoi.java
@@ -1,18 +1,15 @@
+
 class Solution
-{
-    int atoi(String str) {
-	// Your code here
-	    long ans = 0;
-	    boolean neg = false;
-	    for(int i=0;i<str.length();i++){
-	        if(str.charAt(i)=='-' && i==0){
-	            neg = true;
-	        }else if(Character.isDigit(str.charAt(i))){
-	            ans = ans*10+str.charAt(i)-'0';
-	        }else {
-	            return -1;
-	        }
-	    }
-	    return neg == true ? (int)ans*-1 :(int)ans;
+{   
+      int atoi(String s) {
+            boolean minus = s.charAt(0) == '-' ? true : false;
+            int start = s.charAt(0) == '-' ? 1 : 0;
+            int result = 0;
+            for(int i = start ; i<s.length() ; i++) {
+                 if(s.charAt(i) + '0' > 115 || s.charAt(s.length()-1) == '-') return -1;
+                         int val = ('0' - ('a' - s.charAt(i))) + 1;
+                         result = result * 10 + val;
+             }
+        return minus == true ? (-1 * result ) : result ;
     }
 }
