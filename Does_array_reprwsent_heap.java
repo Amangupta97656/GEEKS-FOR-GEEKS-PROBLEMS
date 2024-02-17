75667048375975feff4a87c2ff8706750dae1558
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-02-17 20:38:42 +0530

    Add files via upload

---
+++ Does_array_reprwsent_heap.java
@@ -0,0 +1,12 @@
+class Solution {
+    
+    public boolean countSub(long arr[], long n)
+    {
+        // Your code goes here
+        for(int i=0;i<n;i++){
+            if((2*i+1<n && arr[i]<arr[2*i+1]) || (2*i+2<n&&arr[i]<arr[2*i+2]))
+            return false;
+        }
+        return true;
+    }
+}
