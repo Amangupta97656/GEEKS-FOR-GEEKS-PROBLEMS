acd60d7a04b024b3d0c8e9e4106dea801c1c97d8
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-02-26 20:25:39 +0530

    Add files via upload

---
+++ Power_set.java
@@ -0,0 +1,25 @@
+class Solution
+{
+    public List<String> AllPossibleStrings(String s)
+    {
+        // Code here
+      List<String> a=new ArrayList<>();
+      solve(s,"",a);
+      Collections.sort(a);
+ 
+      return a;
+    }
+    void solve(String s,String ans,List<String> a){
+        if(s.length()==0)
+        {
+            if(!ans.equals(""))
+            a.add(ans);
+            return;
+        }
+        char ch=s.charAt(0);
+        String r=s.substring(1);
+        solve(r,ans,a);
+        solve(r,ans+ch,a);
+        return ;
+    }
+}
