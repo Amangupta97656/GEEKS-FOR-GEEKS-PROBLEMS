3119a4c19fccb599a15694eba7150a0d05e319cc
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-03-05 21:33:23 +0530

    Add files via upload

--- Maximum_index.java
+++ Maximum_index.java
@@ -1,26 +1,28 @@
-class Solution {
-
-    int maxIndexDiff(int arr[], int n) {
-     int right[] = new int[n];
-        int left[] = new int[n];
-        left[0] = arr[0]; 
-        right[n-1] = arr[n-1];
+class Solution{
+    static int maxIndexDiff(int arr[], int n) { 
         
-        for(int i=1; i<n; i++){
-            left[i] = Math.min(arr[i], left[i-1]);
+        int minVal[] = new int[n];
+        int maxVal[] = new int[n];
+        
+        //for min : minVal[i] = min(arr[i],arr[i-1]);
+        minVal[0] = arr[0];
+        for(int i=1;i<n;i++){
+            minVal[i] = Math.min(arr[i],minVal[i-1]);
         }
         
-        for(int j=n-2; j>=0 ; j--){
-            right[j] = Math.max(arr[j], right[j+1]);
+        //for max : maxVal[i] = max(arr[i],maxVal[i+1]);
+        maxVal[n-1] = arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            maxVal[i] = Math.max(arr[i],maxVal[i+1]);
         }
-        
-        int i = 0, j = 0, ans = Integer.MIN_VALUE;
-        while(j < n && i< n){
-            if(left[i] <= right[j]){
-                ans = Math.max(ans, j-i);
+        int i = 0;
+        int j = 0;
+        int ans = Integer.MIN_VALUE;
+        while(i<n && j<n){
+            if(minVal[i]<=maxVal[j]){// Given : a[i] < a[j] and i < j
+                ans = Math.max(ans,j-i);
                 j++;
-            }
-            else{
+            }else{
                 i++;
             }
         }
