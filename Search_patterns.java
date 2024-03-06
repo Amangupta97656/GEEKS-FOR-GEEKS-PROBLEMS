9f146d6e0c93beef51f49abf8f469b44cae2a96d
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-03-06 21:00:34 +0530

    Add files via upload

---
+++ Search_patterns.java
@@ -0,0 +1,33 @@
+class Solution
+
+{
+
+    
+
+    ArrayList<Integer> search(String pattern, String text)
+
+    {
+
+        // your code here
+
+        int x=0;
+
+        ArrayList<Integer> arr = new ArrayList<>() ;
+
+        while(text.indexOf(pattern,x)!=-1)
+
+        {
+
+            x=text.indexOf(pattern,x) ;
+
+            arr.add(x+1);
+
+            x++;
+
+        }
+
+        return arr;
+
+    }
+
+}
