7a4c966ab3c2bfba4372cf3e33731b9c6af78dba
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-04-15 20:02:46 +0530

    Add files via upload

---
+++ Count_the_elements.java
@@ -0,0 +1,31 @@
+class Solution
+{
+    public static int[] countElements(int a[], int b[], int n, int query[], int q)
+    {
+        int[] ans = new int[q];
+        Arrays.sort(b);
+        
+        for(int i=0; i<query.length; i++)
+        {
+            int idx = query[i];
+            int ele = a[idx];
+            
+            int cnt = 0;
+            for(int j=0; j<n; j++)
+            {
+                if(ele-b[j]>=0)
+                {
+                    cnt++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            ans[i] = cnt;
+        }
+        
+        return ans;
+    }
+}
+
