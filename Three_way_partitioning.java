5576ce881979c8943e4631c6b59d7dd8e8c44be0
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-04-21 20:40:05 +0530

    Add files via upload

---
+++ Three_way_partitioning.java
@@ -0,0 +1,8 @@
+class Solution{
+    //Function to partition the array around the range such 
+    //that array is divided into three parts.
+    public void threeWayPartition(int array[], int a, int b)
+    {
+        Arrays.sort(array);
+    }
+}
