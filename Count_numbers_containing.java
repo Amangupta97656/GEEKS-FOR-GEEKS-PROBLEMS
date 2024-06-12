9e64b01ab6dd2168f0ab3e8eb38fe2e3bb821bdc
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-06-12 20:19:10 +0530

    Add files via upload

---
+++ Count_numbers_containing.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static int countNumberswith4(int n) {
+        int count=0;
+       for(int i=1;i<=n;i++){
+           if(Integer.toString(i).contains("4")) count++;
+       }
+       return count;
+    }
+}
+
