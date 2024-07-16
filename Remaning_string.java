f6db5d2adfe7cc58f7f3c1558d6479036a7c44f0
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-07-16 22:20:12 +0530

    Add files via upload

---
+++ Remaning_string.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String printString(String s, char ch, int count) {
+        for(int i = 0;i<s.length();i++){
+            if(count == 0) return s.substring(i);
+            if(s.charAt(i) == ch) count--;
+        }
+        return "";
+    }
+}
