bb02cbc245f63945eae1b4972a41b0325af0ad76
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-08-01 16:23:27 +0530

    Add files via upload

--- Spirally_traversing_a_matrix.java
+++ Spirally_traversing_a_matrix.java
@@ -1,33 +1,55 @@
+class Solution {
+    // Function to return a list of integers denoting spiral traversal of matrix.
+    public ArrayList<Integer> spirallyTraverse(int matrix[][]) {
+        // code here
+         ArrayList<Integer>list=new ArrayList<>();
+        int r=matrix.length;//rows
+        int c=matrix[0].length;//column
+        
+        
+        //create boundaries 
+        int sr=0;
+        int er=r-1;
+        int sc=0;
+        int ec=c-1;
+        
+       while(sr<=er && sc<=ec){
+           
+          //move right
+         for(int j=sc;j<=ec;j++){
+             list.add(matrix[sr][j]);
+         }
+         //row update
+         sr++;
+         
+         //move down 
+         for(int i=sr;i<=er;i++){
+             list.add(matrix[i][ec]);
+         }
+         //column update
+        ec--;
+         
+         //move left
+       if(sr<=er){
+             for(int j=ec;j>=sc;j--){
+             list.add(matrix[er][j]);
+         }
+           er--;
+      
+       }
+     
+         
+         //move up
+        if(sc<=ec){
+             for(int i=er;i>=sr;i--){
+             list.add(matrix[i][sc]);
+         }
+       }
+               sc++;
+        }
 
-class Solution
-{
-    //Function to return a list of integers denoting spiral traversal of matrix.
-    static ArrayList<Integer> spirallyTraverse(int matrix[][], int r, int c)
-    {
-        // code here 
-        ArrayList<Integer>li=new ArrayList<Integer>();
-        int top=0,left=0,right=c-1,bottom=r-1;
-        while(top<=bottom && left<=right){
-            for(int i=left;i<=right;i++){
-            li.add(matrix[top][i]);
-        }
-        top++;
-        for(int i=top;i<=bottom;i++){
-            li.add(matrix[i][right]);
-        }
-        right--;
-        if(!(top<=bottom && left<=right)){
-            break;
-        }
-        for(int i=right;i>=left;i--){
-            li.add(matrix[bottom][i]);
-        }
-        bottom--;
-        for(int i=bottom;i>=top;i--){
-            li.add(matrix[i][left]);
-        }
-        left++;
-        }
-        return li;
+       
+       return list;
+     
     }
 }
