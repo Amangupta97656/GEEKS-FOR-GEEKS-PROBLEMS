2667d42084268e53d29a296ca12c725d71960393
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-08-03 21:16:45 +0530

    Add files via upload

--- The_celebrity_problem.java
+++ The_celebrity_problem.java
@@ -1,26 +1,19 @@
-class Solution
-{ 
-   int celebrity(int M[][], int n) {
-        int x = 0;
-        int y = n - 1; 
-    
-        while (x < y) { 
-            if (M[x][y] == 1) {
-                x++;
-            } else {
-                y--;
-            }
+class Solution {
+    public int celebrity(int[][] mat) {
+        int n = mat.length, j = 0;
+      // j= 0 , means assuming first person as celebrity
+        for(int i=0;i<n;i++){
+      // if j knows i , means that i might be the celebrity 
+            if(knows(mat, j , i)) j = i;
         }
-    
-       
-        for (int i = 0; i < n; i++) {
-            if (i != x) {
-                if (M[x][i] == 1 || M[i][x] == 0) {
-                    return -1;
-                }
-            }
+       // j is celebrity only if j does not know i or every other person know j
+        for(int i=0;i<n;i++){
+            if(j != i && (knows(mat, j, i) || !knows(mat, i, j))) return -1;
         }
-    
-        return x;
+        return j;
+    }
+   // to check whether a knows b or not
+    private boolean knows(int[][] m, int a, int b){
+        return m[a][b] == 1;
     }
 }
