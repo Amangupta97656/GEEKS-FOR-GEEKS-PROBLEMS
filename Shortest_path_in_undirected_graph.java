742543fa7b667d9d0f11e34cbf5fae625b5daa07
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-08-21 21:07:37 +0530

    Add files via upload

--- Shortest_path_in_undirected_graph.java
+++ Shortest_path_in_undirected_graph.java
@@ -1,36 +1,36 @@
 class Solution {
-
-	public int[] shortestPath(int N,int M, int[][] edges) {
-		//Code here
-		List<Map<Integer,Integer>> l=new ArrayList<>();
-		int vis[]=new int[N];
-		for(int i=0;i<N;i++){
-		    l.add(new HashMap<>());
-		    vis[i]=-1;
-		}
-		
-		Map<Integer,Integer> m;
-		for(int i[]:edges){
-		    m=l.get(i[0]);
-		    m.put(i[1],i[2]);
-		}
-		dfs(0,l,vis,0);
-		return vis;
-	}
-	void dfs(int i,List<Map<Integer,Integer>> g,int vis[],int target){
-	    if(vis[i]!=-1){
-	        if(target>vis[i]){
-	            return;
-	        }
-	        else{
-	            vis[i]=Integer.min(target,vis[i]);
-	        }
-	    }
-	    else{
-	        vis[i]=target;
-	    }
-	    for(Map.Entry<Integer,Integer> mp:g.get(i).entrySet()){
-	            dfs(mp.getKey(),g,vis,target+mp.getValue());
-	    }
-	}
+    
+    public int[] shortestPath(int[][] edges,int n,int m ,int src) {
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=0;i<n;i++)    list.add(new ArrayList<>());
+        for(int e[]:edges){
+            list.get(e[0]).add(e[1]);
+            list.get(e[1]).add(e[0]);
+        }
+        
+        
+        int ans[] = new int[n];
+        Arrays.fill(ans,Integer.MAX_VALUE);
+        ans[src]=0;
+        
+        Queue<Integer> q = new LinkedList<>();
+        q.add(src);
+        
+        while(!q.isEmpty()){
+            Integer node = q.remove();
+            int dist = ans[node];
+            
+            for(Integer nbr:list.get(node)){
+                if(ans[nbr]>dist+1){
+                    ans[nbr]=dist+1;
+                    q.add(nbr);
+                }
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            if(ans[i]==Integer.MAX_VALUE)   ans[i]=-1;
+        }
+        return ans;
+    }
 }
