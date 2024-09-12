d984e1d2d95bb2a3272330fe43d6d92d7dec0984
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-09-12 21:43:49 +0530

    Add files via upload

--- Finding_middle_element_in_a_linked_list.java
+++ Finding_middle_element_in_a_linked_list.java
@@ -1,23 +1,12 @@
-class Solution
-{
-    int getMiddle(Node head)
-    {
-         // Your code here.
-         if(head==null)
-          return -1;
-          Node temp=head;
-          int len =0;
-          while(temp!=null)
-          {
-              len++;
-              temp=temp.next;
-          }
-          len=len/2;
-          Node curr=head;
-          for(int i=0;i<len;i++)
-             {
-                 curr=curr.next;
-             }
-          return curr.data;  
+class Solution {
+    int getMiddle(Node head) {
+        // Your code here.
+        int size=0;
+        for(Node curr=head;curr!=null;curr=curr.next)
+        {
+            size++;
+        }
+        for(int i=0;i<size/2;i++)head=head.next;
+        return head.data;
     }
 }
