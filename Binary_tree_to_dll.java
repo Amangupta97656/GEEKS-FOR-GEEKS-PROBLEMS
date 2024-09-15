935ea18634fafb4172ac4744a9e2e44a5e373e93
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-09-15 20:54:54 +0530

    Add files via upload

--- Binary_tree_to_dll.java
+++ Binary_tree_to_dll.java
@@ -1,29 +1,27 @@
-
-class Solution
-{   static Node h=null;
-Node prev=null;
-private void inorder(Node root)
-{
-if(root==null)
-return;
-inorder(root.left);
- 
-if(prev!=null)
-{
-root.left=prev;
-prev.right=root;
- 
-}
-else
-h=root;
-prev=root;
-inorder(root.right);
-}
-    //Function to convert binary tree to doubly linked list and return it.
-    Node bToDLL(Node root)
-    {
-inorder(root);
-return h;
- 
+class Solution {
+    Node prev = null;
+    
+    Node bToDLL(Node root) {
+    	if(root == null){
+    	    return null;
+    	}
+    	
+    	Node head = null;
+    	
+    	head = bToDLL(root.left);
+    	
+    	if(prev==null){
+    	    head = root;
+    	}
+    	else{
+    	    prev.right = root;
+    	    root.left = prev;
+    	}
+    	
+    	prev = root;
+    	
+    	bToDLL(root.right);
+    	
+    	return head;
     }
 }
