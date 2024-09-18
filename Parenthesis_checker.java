5ae77c5df0245256dced0c2256c75b4607be0aae
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-09-18 21:28:07 +0530

    Add files via upload

--- Parenthesis_checker.java
+++ Parenthesis_checker.java
@@ -3,79 +3,29 @@ class Solution
     //Function to check if brackets are balanced or not.
     static boolean ispar(String x)
     {
-        if(x.length()%2 != 0) {
-
-            return false;
-
-        }
-
-        
-
-        Stack<Character> stack = new Stack<>();
-
-        
-
-        for(int i=0; i<x.length(); i++) {
-
-            
-
-            char ch = x.charAt(i);
-
-            boolean found = false;
-
-            if(ch == ')') {
-
-                if(!stack.isEmpty() && stack.peek() == '(') {
-
-                    stack.pop();
-
-                    found = true;
-
-                }
-
-            }
-
-            else if(ch == '}') { 
-
-                if(!stack.isEmpty() && stack.peek() == '{') {
-
-                    stack.pop();
-
-                    found = true;
-
-                }
-
-            }
-
-            else if(ch == ']') {
-
-                if(!stack.isEmpty() && stack.peek() == '[') {
-
-                    stack.pop();
-
-                    found = true;
-
-                }
-
-            }
-
-            if(!found)
-
+        // add your code here
+        Stack<Character>stack=new Stack<>();
+        for(int i=0;i<x.length();i++){
+            char ch=x.charAt(i);
+            if(ch=='{'||ch=='('||ch=='['){
                 stack.push(ch);
-
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }else if(ch==')'&&stack.peek()=='('){
+                    stack.pop();
+                }else if(ch=='}'&&stack.peek()=='{'){
+                    stack.pop();
+                }else if(ch==']'&&stack.peek()=='['){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            }
+            // System.out.println("stack="+stack);
         }
-
-        
-
-        if(stack.size() == 0) {
-
-            return true;
-
-        }
-
-        
-
-        return false;
-
+        if(!stack.isEmpty()) return false;
+        return true;
     }
-    }
+}
+
