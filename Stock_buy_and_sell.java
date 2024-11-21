b732a08d5889cb0418cd9b4142bdfcb48612a745
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-11-21 20:19:30 +0530

    Add files via upload

---
+++ Stock_buy_and_sell.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maximumProfit(int prices[]) {
+        // code here
+        int profit = 0;
+        for(int i=1; i<prices.length; i++){
+            if(prices[i]>prices[i-1]){
+                profit = profit+prices[i] - prices[i-1];
+            }
+        }
+        return profit;
+    }
+}
