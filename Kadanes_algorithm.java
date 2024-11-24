d3b36e21822d89abb1066d1869e6173f9facd21c
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-11-24 18:22:34 +0530

    Add files via upload

--- Kadanes_algorithm.java
+++ Kadanes_algorithm.java
@@ -1,20 +1,27 @@
 class Solution {
-
-    // arr: input array
-    // Function to find the sum of contiguous subarray with maximum sum.
     int maxSubarraySum(int[] arr) {
         // Your code here
-        int max = Integer.MIN_VALUE;
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum = sum + arr[i];
-            if (sum > max) {
-                max = sum;
+        int n =  arr.length;
+        long maxsum = Integer.MIN_VALUE;
+        long currsum = 0;
+        
+        for(int i = 0 ;i < n; i++)
+        {
+            currsum += arr[i];
+            if(currsum < 0)
+            {
+                currsum = 0;
             }
-            if (sum < 0) {
-                sum = 0;
-            }
+            maxsum = Math.max(maxsum,currsum);
         }
-        return max;
+        if(maxsum > 0)
+        {
+            return (int)maxsum;
+        }
+        else
+        {
+            Arrays.sort(arr);
+            return arr[n-1];
+        }
     }
 }
