88f2d4e27697e26f2dfe72a6f8896880ffc1cbaa
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2024-12-31 20:17:34 +0530

    Add files via upload

--- Longest_consecutive_subsequence.java
+++ Longest_consecutive_subsequence.java
@@ -1,34 +1,32 @@
-class Solution
-{   
-    // arr[] : the input array
-    // N : size of the array arr[]
-    
-    //Function to return length of longest subsequence of consecutive integers.
-    static int findLongestConseqSubseq(int arr[], int N)
-    {
-       // add your code here
-       int maxi=0;
-       Arrays.sort(arr);
-       int lastS=Integer.MIN_VALUE,c=0;
-       for(int i=0;i<N-1;i++)
-       {
-           if(arr[i+1]==arr[i])
-           {
-               continue;
-           }
-           else if(arr[i+1]==arr[i]+1)
-           {
-               lastS=arr[i+1];
-               c++;
-               maxi= Math.max(maxi,c);
-               
-           }
-           else
-           {
-               c=0;
-           }
-       }
-       return maxi+1;
+class Solution {
+
+    // Function to return length of longest subsequence of consecutive integers.
+    public int longestConsecutive(int[] arr) {
+        // code here
+        Arrays.sort(arr);
+        int currentlength=1;
+        int maxlength=1;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]==arr[i-1]+1){
+                currentlength++;
+                
+            }
+            else if (arr[i]==arr[i-1])
+               {
+                    continue;
+               }
+               else{
+                    maxlength=Math.max(maxlength,currentlength);
+                    currentlength=1;
+                }
+                
+                
+            }
+           maxlength=Math.max(maxlength,currentlength);
+           return maxlength;
+            
+            
+        
+        
     }
 }
-
