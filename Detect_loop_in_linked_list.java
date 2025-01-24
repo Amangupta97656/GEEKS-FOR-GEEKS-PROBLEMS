7da3d56516945ec1b69f2f796c1d8b53aaf1f17c
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-01-24 22:26:29 +0530

    Add files via upload

--- Detect_loop_in_linked_list.java
+++ Detect_loop_in_linked_list.java
@@ -1,23 +1,14 @@
 class Solution {
-    //Function to check if the linked list has a loop.
-    public static boolean detectLoop(Node head){
-          if(head == null || head.next == null){
-            return false;  //Presence of one single node in the Linked List 
-        } 
-        
-        
-        Node slow = head;
-        Node fast = head;
-        
-        while(fast.next != null && fast.next.next != null){
-            fast = fast.next.next;
-            slow = slow.next;
-            
-            if(fast == slow){
-                return true;
-            }
+    // Function to check if the linked list has a loop.
+    public static boolean detectLoop(Node head) {
+        // Add code here
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast) return true;
         }
-        
         return false;
     }
 }
