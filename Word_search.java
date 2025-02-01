e3de5d4b95e62ea8edce7beeb104dc2030c33408
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-02-01 22:53:05 +0530

    Add files via upload

--- Word_search.java
+++ Word_search.java
@@ -1,35 +1,45 @@
-class Solution{
-    public boolean isWordExist(char[][] board, String word){
-        // Code here
-        int row = board.length;
-        int col = board[0].length;
-        int[][] visited = new int[row][col];
-        int k=0;
+class Solution {
+    
+    static int[] dx = {0,1,0,-1};
+    static int[] dy = {1,0,-1,0};
+    static boolean[][] visited; // visited cells
+    
+    // backTrack from cell (i,j) 
+    static public boolean backTrack(char[][] mat, int i , int j , String word , int index) {
+        if(index == word.length()) return true; // if reached end of string , ie formed word
         
-        for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++){
-                if(find(board, word, i, j, row, col, k, visited))
+        // if (i,j) goes OUT OF BOUNDS of grid OR
+        // (i,j) DOES NOT match with word[index] ( ie next character in word) OR
+        // (i,j) is ALREADY visited 
+        if(i < 0 || i >= mat.length || j < 0 || j >= mat[0].length || word.charAt(index) != mat[i][j] || visited[i][j] == true ) return false;
+        
+        visited[i][j] = true; // mark (i,j) as visited
+        // go 4-directionally from (i,j) to see if we find a path for our target word
+        boolean ans =  
+                   backTrack(mat , i + dx[0] , j + dy[0] , word , index + 1) ||
+                   backTrack(mat , i + dx[1] , j + dy[1] , word , index + 1) ||
+                   backTrack(mat , i + dx[2] , j + dy[2] , word , index + 1) ||
+                   backTrack(mat , i + dx[3] , j + dy[3] , word , index + 1);
+
+        visited[i][j] = false; // mark (i,j) as false when bactrack returns
+        return ans;
+    }
+    
+    
+    static public boolean isWordExist(char[][] mat, String word) {
+        // Code here
+        int rows = mat.length;
+        int cols = mat[0].length;
+        visited = new boolean[rows][cols]; // visited array
+        
+        // try forming word from each (i,j) cell 
+        for(int i = 0 ; i < rows ; i++) {
+            for(int j = 0 ; j < cols ; j++) {
+                if(backTrack(mat , i , j , word , 0) == true) {
                     return true;
+                }
             }
         }
         return false;
     }
-    
-    public boolean find(char[][] board, String word, int i, int j, int row, int col, int k, int[][] visited){
-        if(k==word.length())
-            return true;
-            
-        if(i<0 || i>=row || j<0 || j>=col || visited[i][j]==1 || board[i][j]!=word.charAt(k))
-            return false;
-            
-        visited[i][j]=1; 
-        boolean up = find(board, word, i-1, j, row, col, k+1, visited);
-        boolean down = find(board, word, i+1, j, row, col, k+1, visited);
-        boolean left = find(board, word, i, j-1, row, col, k+1, visited);
-        boolean right = find(board, word, i, j+1, row, col, k+1, visited);
-        visited[i][j]=0;
-        
-        return up || down || left || right;
-                
-    }
-}
+} 
