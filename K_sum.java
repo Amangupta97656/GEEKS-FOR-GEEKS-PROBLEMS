07970b6817c9bc99cf6f06d242d2ff5eb29c89ed
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-02-10 22:14:36 +0530

    Update K_sum.java

--- K_sum.java
+++ K_sum.java
@@ -1,13 +1,26 @@
 class Solution {
-    public String clearDigits(String s) {
-        String ans="";
-        String num="0123456789";
-        for(int i=0;i<s.length();i++)
-        {
-            if(num.contains((s.charAt(i)+"")))
-            {ans=ans.substring(0,ans.length()-1);}
-            else{ans+=s.charAt(i);}
+        int count = 0;
+    public int sumK(Node root, int k) {
+        // code here
+        ArrayList<Integer> path = new ArrayList<>();
+        helper(root, k, path);
+        return count;
+    }
+    
+    void helper(Node root, int k, ArrayList<Integer> path){
+        if(root == null ) return;
+        
+        path.add(root.data);
+        int sum = 0;
+        
+        for(int i = path.size()-1; i>=0; i--){
+            sum += path.get(i);
+            if(sum == k) count++;
         }
-        return ans;
+        
+        helper(root.left, k, path);
+        helper(root.right, k, path);
+        
+        path.remove(path.size()-1);
     }
 }
