b86bd7bff26b17277873df503301ec4425267323
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-03-17 22:20:49 +0530

    Add files via upload

--- Subset_sum_problem.java
+++ Subset_sum_problem.java
@@ -1,24 +1,29 @@
-class Solution{
-     static Boolean isSubsetSum(int N, int arr[], int sum){
-        // code here
-        boolean dp[][]=new boolean[N+1][sum+1];
-        int i,j;
-        for(i=0;i<N+1;i++){
-            for(j=0;j<sum+1;j++){
-                if(i==0)
-                    dp[i][j]=false;
-                if(j==0)
-                    dp[i][j]=true;
-            }
+class Solution {
+
+    public static boolean fun(int arr[],int n,int sum,Boolean dp[][])
+    {
+        if(sum==0)
+        {
+            return true;
         }
-        for(i=1;i<N+1;i++){
-            for(j=1;j<sum+1;j++){
-                if(arr[i-1]<=j)
-                    dp[i][j]=dp[i-1][j-arr[i-1]]||dp[i-1][j];
-                else
-                    dp[i][j]=dp[i-1][j];
-            }
+        if(n==0)
+        {
+            return false;
         }
-        return dp[N][sum];
+        if(dp[n][sum]!=null)
+        {
+            return dp[n][sum];
+        }
+        if(arr[n-1]>sum)
+        {
+            return dp[n][sum]=fun(arr,n-1,sum,dp);
+        }
+        return dp[n][sum] = fun(arr,n-1,sum,dp) || fun(arr,n-1,sum-arr[n-1],dp);
+    }
+    static Boolean isSubsetSum(int arr[], int sum) {
+        int n=arr.length;
+        Boolean dp[][]=new Boolean[n+1][sum+1];
+        return fun(arr,n,sum,dp);
+        
     }
 }
