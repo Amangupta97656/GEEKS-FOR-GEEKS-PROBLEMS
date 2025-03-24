f2c5dee70a9b77d0df039af80eca5ec3b92a0692
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-03-24 21:04:23 +0530

    Add files via upload

--- Matrix_chain_multiplication.java
+++ Matrix_chain_multiplication.java
@@ -1,36 +1,20 @@
-class Solution{
-    static int mcm(int arr[],int i,int j,int dp[][])
-    {
-        if(i==j)
-        {
-            return 0;
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        
-        int ans=Integer.MAX_VALUE;
-        for(int k=i;k<=j-1;k++)
-        {
-            int cost1=mcm(arr,i,k,dp);
-            int cost2=mcm(arr,k+1,j,dp);
-            int cost3=arr[i-1]*arr[k]*arr[j];
-            int finalcost=cost1+cost2+cost3;
-            ans=Math.min(ans,finalcost);
-            
-        }
-        return dp[i][j]=ans;
-    }
-    static int matrixMultiplication(int N, int arr[])
-    {
+class Solution {
+    static int matrixMultiplication(int arr[]) {
         // code here
-        int dp[][]=new int[N][N];
-        for(int i=0;i<N;i++)
-        {
-            Arrays.fill(dp[i],-1);
+            int n=arr.length;
+        int dp[][]=new int[n][n];
+        for(int i=0;i<n;i++)dp[i][i]=0;
+        for(int len=2;len<n;len++){
+            for(int i=1;i<n-len+1;i++){
+                int j=i+len-1;
+                dp[i][j]=Integer.MAX_VALUE;
+                for(int k=i;k<j;k++){
+                    int cost=dp[i][k]+dp[k+1][j]+arr[i-1]*arr[j]*arr[k];
+                    dp[i][j]=Math.min(dp[i][j],cost);
+                }
+            }
         }
-        int a=mcm(arr,1,N-1,dp);
-        return a;
+        return dp[1][n-1];
     }
 }
+
