c226c919be56f737cefe5e4a96de8f22ced5efff
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-05-08 19:04:36 +0530

    Add files via upload

---
+++ Missing_element_of_ap.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findMissing(int[] arr) {
+        // code here
+        int n=arr.length;
+        int diff=arr[1]-arr[0];
+        for(int i =2;i<n;i++){
+            if(arr[i-1]!=arr[i]-diff){
+                return arr[i]-diff;
+            }
+        }
+        return arr[n-1]+diff;
+    }
+}
