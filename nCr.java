8b2be98da2d5db2b60fd647daa52149e69ffd462
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-05-13 18:11:38 +0530

    Add files via upload

--- nCr.java
+++ nCr.java
@@ -1,27 +1,14 @@
-class Solution{
-    static int nCr(int n, int r)
-    {
-       if(r>n)
-            return 0;
-       int mod = 1000000007;
-       int[][] dp = new int[n+1][n+1];
-       for(int i = 0;i<=n;i++)
-       {
-           for(int j = 0;j<=n;j++)
-           {
-               if(i == j || j == 0)
-               {
-                   dp[i][j] = 1;
-               }
-           }
-       }
-       for(int i = 1;i<=n;i++)
-       {
-           for(int j = 1;j<i;j++)
-           {
-              dp[i][j] = (dp[i-1][j-1]+dp[i-1][j])%mod;
-           }
-       }
-       return (dp[n][r])%mod;
+class Solution {
+    public int nCr(int n, int r) {
+        // code here
+        if(r>n) return 0;
+        
+        long res=1;
+        for(int i=0;i<r;i++){
+            res=res*(n-i);
+            res=res/(i+1);
+        }
+        
+        return (int)res;
     }
 }
