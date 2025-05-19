4c053304c72faa5f6cbc8eaa0d83dca6fe9befff
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-05-19 22:18:01 +0530

    Add files via upload

--- Predecessor_and_successor.java
+++ Predecessor_and_successor.java
@@ -1,31 +1,38 @@
-static ArrayList<Node> list;
-    public static void inorderTraversal(Node root)
-    {
-        if(root==null)
-            return;
-        list.add(root);
-        inorderTraversal(root.left);
-        inorderTraversal(root.right);
-    }
-    public static void findPreSuc(Node root, int key)
-    {
-        list=new ArrayList<Node>();
-        inorderTraversal(root);
-        int preTemp = Integer.MAX_VALUE,sucTemp=Integer.MAX_VALUE;
-        for(int i=0;i<list.size();i++)
-        {
-            Node tempNode = list.get(i);
-            int calc=Math.abs(key-tempNode.data);
-            if(key>tempNode.data && calc<preTemp)
-            {
-                preTemp=calc;
-                pre=tempNode;
-            }
-            else if(key<tempNode.data && calc<sucTemp)
-            {
-                sucTemp=calc;
-                suc=tempNode;
+class Solution {
+    public ArrayList<Node> findPreSuc(Node root, int key) {
+        // code here
+        ArrayList<Node> result = new ArrayList<>();
+        Node predecessor = null;
+        Node successor = null;
+        while (root != null) {
+            if (root.data == key) {
+                if (root.left != null) {
+                    Node temp = root.left;
+                    while (temp.right != null) {
+                        temp = temp.right;
+                    }
+                    predecessor = temp;
+                }
+                if (root.right != null) {
+                    Node temp = root.right;
+                    while (temp.left != null) {
+                        temp = temp.left;
+                    }
+                    successor = temp;
+                }
+                break;
+            } else if (root.data < key) {
+                predecessor = root;
+                root = root.right;
+            } else {
+                successor = root;
+                root = root.left;
             }
         }
+        result.add(predecessor);
+        result.add(successor);
+        return result;
+    
     }
 }
+
