43e7250fbb4c9125403663b805bfe12586cfc2d9
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-05-20 16:17:00 +0530

    Add files via upload

--- Burning_tree.java
+++ Burning_tree.java
@@ -1,48 +1,45 @@
 class Solution {
-    static int BFS(Map<Integer, HashSet<Integer>> adj, int src) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        Set<Integer> visited = new HashSet<>();
-        int count = 0;
-        
-        queue.offer(src);
-        visited.add(src);
-        
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            count++;
-            for (int i = 0; i < size; i++) {
-                int node = queue.poll();
-                for (int neighbor : adj.get(node)) {
-                    if (!visited.contains(neighbor)) {
-                        visited.add(neighbor);
-                        queue.offer(neighbor);
-                    }
-                }
-            }
-        }
-        return count - 1;
-    }
-    
-    static void createGraph(Node root, Map<Integer, HashSet<Integer>> adj) {
-        if (root == null) return;
-        
-        if (root.left != null) {
-            adj.computeIfAbsent(root.data, k -> new HashSet<>()).add(root.left.data);
-            adj.computeIfAbsent(root.left.data, k -> new HashSet<>()).add(root.data);
-            createGraph(root.left, adj);
-        }
-        
-        if (root.right != null) {
-            adj.computeIfAbsent(root.data, k -> new HashSet<>()).add(root.right.data);
-            adj.computeIfAbsent(root.right.data, k -> new HashSet<>()).add(root.data);
-            createGraph(root.right, adj);
-        }
-    }
-
+    static int maxTime = 0;
     public static int minTime(Node root, int target) {
-        if(root.left == null && root.right == null) return 0;
-        Map<Integer, HashSet<Integer>> adj = new HashMap<>();
-        createGraph(root, adj);
-        return BFS(adj, target);
+        // code here
+        maxTime = 0;
+        burnTree(root, target);
+        return maxTime;
+    }
+    private static int burnTree(Node node, int target){
+        // Target node is not encountered
+        if(node == null){
+            return -1;
+        }
+        if(node.data == target){
+            burnSubTree(node, 0);
+            return 1;
+        }
+        
+        int left = burnTree(node.left, target);
+        if(left != -1){
+            // If fire came from left burn right sub tree
+            burnSubTree(node.right, left+1);
+            maxTime = Math.max(maxTime, left);
+            return left+1;
+        }
+        
+        int right = burnTree(node.right, target);
+        if(right != -1){
+            // If fire came from right burn left sub tree
+            burnSubTree(node.left, right+1);
+            maxTime = Math.max(maxTime, right);
+            return right+1;
+        }
+        
+        return -1; // Target Node not found
+    }
+    private static void burnSubTree(Node node, int time){
+        if(node == null){
+            return;
+        }
+        maxTime = Math.max(maxTime, time);
+        burnSubTree(node.left, time+1);
+        burnSubTree(node.right, time+1);
     }
 }
