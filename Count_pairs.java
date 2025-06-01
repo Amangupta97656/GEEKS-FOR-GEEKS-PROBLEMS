6a0351b82516b9af19e56cabcda7d37f701dab30
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-06-01 10:36:40 +0530

    Add files via upload

--- Count_pairs.java
+++ Count_pairs.java
@@ -1,17 +1,25 @@
 class Solution {
-    int countPairs(int arr[], int target) {
-        // Your code here
-        Arrays.sort(arr);
+    int countPairs(int[][] mat1, int[][] mat2, int x) {
+        // code here
+        int n=mat1.length;
         int count=0;
-        int st=0;
-        int ed=arr.length-1;
-        while(st<ed){
-            if((arr[st]+arr[ed])<target){
-                count+=ed-st;
-                st++;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                map.put(mat2[i][j],map.getOrDefault(0,1)+1);
             }
-            else{
-            ed--;
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int remaining=x-mat1[i][j];
+                if(map.containsKey(remaining))
+                {
+                    count=count+1;
+                }
             }
         }
         return count;
