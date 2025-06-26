ae793dd6e7c0f3ffcaf732455e3447534f628271
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-06-26 19:32:46 +0530

    Add files via upload

--- Game_with_string.java
+++ Game_with_string.java
@@ -1,43 +1,30 @@
-class Solution{
-    static int minValue(String s, int k){
-     
-     
-     HashMap<Character,Integer> hs=new HashMap<>();
-     
-     for(int i=0;i<s.length();i++)
-     {
-         if(hs.containsKey(s.charAt(i)))
-         {
-             hs.put(s.charAt(i),hs.get(s.charAt(i))+1);
-         }
-         else
-         {
-             hs.put(s.charAt(i),1);
-         }
-     }
-     int arr[]=new int[hs.size()];
-     
-     int index=0;
-     for(char a:hs.keySet())
-     {
-         arr[index]=hs.get(a);
-         index++;
-     }
-   
-     Arrays.sort(arr);
-     int ans=0;
-     int j=arr.length-1;
-     while(k!=0)
-    {
-     arr[j]= arr[j]-1;
-     Arrays.sort(arr);
-     k--;
-    }
-     for(int i=0;i<arr.length;i++)
-     {
-         ans+=Math.pow(arr[i],2);
-     }
-     return ans;
-     
+class Solution {
+    public int minValue(String s, int k) {
+        // code here
+        int[] freq = new int[26];
+        for(char c : s.toCharArray()){
+            freq[c - 'a']++;
+        }
+        
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for(int f : freq){
+            if(f > 0) maxHeap.add(f);
+        }
+        
+        while(k-- > 0  && !maxHeap.isEmpty()){
+            int top = maxHeap.poll();
+            if(top > 1){
+                maxHeap.add(top - 1);
+            }
+        }
+        
+        int result = 0;
+        
+        while(!maxHeap.isEmpty()){
+            int f = maxHeap.poll();
+            result += f*f;
+        }
+        
+        return result;
     }
 }
