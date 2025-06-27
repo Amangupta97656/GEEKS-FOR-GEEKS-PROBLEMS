4f7fb0a250a7b02af1cae8537c471e74c4fea3d2
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-06-27 22:18:44 +0530

    Add files via upload

--- Mobile_numeric_keypad.java
+++ Mobile_numeric_keypad.java
@@ -1,40 +1,27 @@
 class Solution {
-    public long getCount(int n) {
-        if(n==1)
-        return 10;
-        
-        long[][] prev={
-            {1,1,1},
-            {1,1,1},
-            {1,1,1},
-            {0,1,0},
-        };
-        
-        long ans=0;
-        long[][] cur=new long[4][3];
-        for(int i=1;i<n;i++){
-            long sum=0;
-            for(int row=0;row<4;row++){
-                for(int col=0;col<3;col++){
-                    if(prev[row][col]==0)
-                        continue;
-                    cur[row][col]=prev[row][col];
-                    if(row-1>=0)
-                        cur[row][col]+=prev[row-1][col];
-                    if(row+1<4)
-                        cur[row][col]+=prev[row+1][col];
-                    if(col-1>=0)
-                        cur[row][col]+=prev[row][col-1];
-                    if(col+1<3)
-                        cur[row][col]+=prev[row][col+1];
-                    sum+=cur[row][col];
-                }
+    public int getCount(int n) {
+        // code here
+        int dialPad[][]=new int[][]{{1,2,3},{4,5,6},{7,8,9},{-1,0,-1}};
+        int ans=0;
+        for(int i=0;i<4;i++){
+            for(int j=0;j<3;j++){
+                ans+=rec(i,j,dialPad,n,new HashMap<>());
             }
-            long temp[][]=prev;
-            prev=cur;
-            cur=temp;
-            ans=sum;
         }
         return ans;
+    }
+    private int rec(int i,int j,int dial[][],int n,Map<String,Integer> hm){
+        if(i<0 || j<0 || i==4 || j==3 || dial[i][j]==-1) return 0;
+        String key=i+" "+j+" "+n;
+        if(hm.containsKey(key)) return hm.get(key);
+        int curr=0;
+        if(n==1) return 1;
+        curr+=rec(i,j,dial,n-1,hm);
+        curr+=rec(i-1,j,dial,n-1,hm);
+        curr+=rec(i+1,j,dial,n-1,hm);
+        curr+=rec(i,j+1,dial,n-1,hm);
+        curr+=rec(i,j-1,dial,n-1,hm);
+        hm.put(key,curr);
+        return curr;
     }
 }
