34743527bb74d912645b2c48278e8f2cbc6c24f5
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-06-30 21:58:14 +0530

    Add files via upload

--- Max_min_height.java
+++ Max_min_height.java
@@ -1,70 +1,51 @@
-class Solution{
-	long maximizeMinHeight(int n, int k, int w,int [] a)
-    {
-         long left = Arrays.stream(a).min().getAsInt();
+class Solution {
+    public static boolean isPossible(int[] arr, int n, int k, int w, int target) {
+        int[] diff = new int[n + 1];
+        long used = 0;
+        int add = 0;
 
-        long right = left + k + 1;
+        for (int i = 0; i < n; i++) {
+            add += diff[i];
+            int currHeight = arr[i] + add;
 
-        while (left < right) {
+            if (currHeight < target) {
+                int need = target - currHeight;
+                used += need;
+                if (used > k) return false;
 
-            long mid = (left + right + 1) / 2;
-
-            if (canAchieveMinHeight(mid, a, k, w)) {
-
-                left = mid;
-
-            } else {
-
-                right = mid - 1;
-
-            }
-
-        }
-
-        return left;
-
-    }
-
- 
-
-    boolean canAchieveMinHeight(long minHeight, int[] a, int k, int w) {
-
-        long[] mods = new long[a.length];
-
-        long add = 0;
-
-        for (int i = 0; i < a.length; i++) {
-
-            add += mods[i];
-
-            long v = a[i] + add;
-
-            if (v < minHeight) {
-
-                long delta = minHeight - v;
-
-                if (delta > k) {
-
-                    return false;
-
+                add += need;
+                if (i + w < n) {
+                    diff[i + w] -= need;
                 }
-
-                if (i + w < a.length) {
-
-                    mods[i + w] -= delta;
-
-                }
-
-                add += delta;
-
-                k -= delta;
-
             }
-
         }
 
         return true;
-
     }
 
+    public static int maxMinHeight(int[] arr, int k, int w) {
+        int n = arr.length;
+        int low = Integer.MAX_VALUE;
+        int high = 0;
+
+        for (int height : arr) {
+            low = Math.min(low, height);
+            high = Math.max(high, height);
+        }
+
+        high += k;
+        int answer = low;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (isPossible(arr, n, k, w, mid)) {
+                answer = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return answer;
+    }
 }
