308e10774cd8c26e09e36c879f22f4afe13dc8c9
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-07-26 22:58:00 +0530

    Add files via upload

--- Majority_vote.java
+++ Majority_vote.java
@@ -1,20 +1,22 @@
 class Solution {
-    public List<Integer> findMajority(int[] nums) {
-        int n = nums.length;
-        int oneThird = n / 3;
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-        List<Integer> result = new ArrayList<>();
-
-        for (int num : nums) {
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+    public ArrayList<Integer> findMajority(int[] arr) {
+        // Code here
+        Map<Integer ,Integer> m=new HashMap<>();
+        int n=arr.length/3;
+         ArrayList<Integer> l=new  ArrayList<>();
+        for(int a:arr){
+            m.put(a,m.getOrDefault(a,0)+1);
         }
-
-        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-            if (entry.getValue() > oneThird) {
-                result.add(entry.getKey());
+        
+        for(int k:m.keySet()){
+            
+            if(m.get(k)>n){
+                l.add(k);
             }
         }
-
-        return result;
+        
+          Collections.sort(l);
+        
+        return l;
     }
 }
