2eb66cdcf6ffd4046eaa883dde51580f6fcad7bc
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-09-08 12:17:50 +0530

    Add files via upload

---
+++ Merge_sort_for_ll.java
@@ -0,0 +1,25 @@
+class Solution {
+    public Node mergeSort(Node Head) {
+        // code here
+        //O(N Log K)
+        PriorityQueue<Integer>pq=new PriorityQueue<>();
+        Node head=Head;
+        while(head!=null){
+            pq.offer(head.data);
+            head=head.next;
+        }
+        Node front=new Node(pq.poll());
+        Node tail=front;
+        while(!pq.isEmpty()){
+            Node node=new Node(pq.poll());
+            if(front.next==null){
+                front.next=node;
+                tail=node;
+            }else{
+                tail.next=node;
+                tail=tail.next;
+            }
+        }
+        return front;
+    }
+}
