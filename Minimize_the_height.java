f8d9c9ef010e886068896320e332efc62ef7a1ca
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-09-12 22:11:18 +0530

    Add files via upload

--- Minimize_the_height.java
+++ Minimize_the_height.java
@@ -1,14 +1,24 @@
 class Solution {
-    public int getMinDiff(int k, int[] arr) {
-        int n=arr.length;
-        Arrays.sort(arr);
-        int ans=arr[n-1]-arr[0];
-        for(int i=1;i<n;i++){
-                int max=Math.max(arr[i-1]+k,arr[n-1]-k);
-                int min=Math.min(arr[0]+k,arr[i]-k);
-                ans=Math.min(ans,max-min);
-        }
-        return ans;
+    public int getMinDiff(int[] arr, int k) {
+        // code here
+        
+    int n = arr.length;
+    Arrays.sort(arr);
+
+    int ans = arr[n - 1] - arr[0];
+    int smallest = arr[0] + k;
+    int largest = arr[n - 1] - k;
+
+    for (int i = 0; i < n - 1; i++) {
+        int minVal = Math.min(smallest, arr[i + 1] - k);
+        int maxVal = Math.max(largest, arr[i] + k);
+
+        if (minVal < 0) continue;
+
+        ans = Math.min(ans, maxVal - minVal);
     }
+    return ans;
+}
+
 }
 
