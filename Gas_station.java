934e0ea7894659d0e610dd42e155d029f2f495b5
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-09-14 23:27:18 +0530

    Add files via upload

--- Gas_station.java
+++ Gas_station.java
@@ -1,23 +1,33 @@
 class Solution {
     public int startStation(int[] gas, int[] cost) {
-        int n = gas.length;
-        int totalGas = Arrays.stream(gas).sum();
-        int totalCost = Arrays.stream(cost).sum();
-        
-        if (totalGas < totalCost) return -1; 
-        int start = 0;
-        int balance = 0;
-        int deficit = 0;
-        
-        for (int i = 0; i < n; i++) {
-            balance += gas[i] - cost[i];
-            if (balance < 0) {
-                start = i + 1; 
-                deficit += balance; 
-                balance = 0;
+        // code here
+    
+        int totalGas = 0, totalCost = 0;
+        int start = 0, tank = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+            tank += gas[i] - cost[i];
+
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
             }
         }
-        
-        return (balance + deficit >= 0) ? start : -1; 
+
+        return (totalGas < totalCost) ? -1 : start;
     }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] gas1 = {4, 5, 7, 4};
+        int[] cost1 = {6, 6, 3, 5};
+        System.out.println(sol.startStation(gas1, cost1)); // Output: 2
+
+        int[] gas2 = {3, 9};
+        int[] cost2 = {7, 6};
+        System.out.println(sol.startStation(gas2, cost2)); // Output: -1  
+     }
 }
