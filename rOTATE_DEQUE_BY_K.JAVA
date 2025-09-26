efa11601821ee1e94e43baada94240fb75297122
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-09-26 22:17:26 +0530

    Add files via upload

---
+++ rOTATE_DEQUE_BY_K.JAVA
@@ -0,0 +1,17 @@
+class Solution {
+    public static void rotateDeque(Deque<Integer> dq, int type, int k) {
+        // code here
+        if(type == 1) {
+            while (k != 0) {
+                dq.addFirst(dq.removeLast());
+                k--;
+            }
+        }else {
+            while (k != 0){
+                dq.addLast(dq.removeFirst());
+                k--;
+            }
+        }
+        
+    }
+}
