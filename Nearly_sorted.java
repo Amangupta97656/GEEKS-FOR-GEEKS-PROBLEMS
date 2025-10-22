556944a29894c22c05fd7bc11ba1bb50dbe5a025
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-10-22 21:15:16 +0530

    Add files via upload

--- Nearly_sorted.java
+++ Nearly_sorted.java
@@ -1,17 +1,24 @@
 class Solution {
-    // Non-static method, so you need to call it on an instance of the class
     public void nearlySorted(int[] arr, int k) {
-        // code
-        int n = arr.length;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // code here
+         PriorityQueue<Integer> pq = new PriorityQueue<>(); // min heap
         
-        for(int a : arr){
-            pq.offer(a);
+        for(int i=0; i<=k; i++){
+            // insert k elements from starting into pq
+            pq.offer(arr[i]);
         }
         
-        for(int i = 0;i < n;i++){
-            arr[i] = pq.poll();
+        int index = 0; // for filling up the arr in sorted order
+        for(int i=k+1; i<arr.length; i++){
+            arr[index++] = pq.poll(); // extract min and store in arr
+            pq.add(arr[i]); // also store the next element in pq for further processing
         }
         
+        while(!pq.isEmpty()){
+            // store the remaining elements to arr
+            arr[index++] = pq.poll();
+        }
+    
     }
 }
+
