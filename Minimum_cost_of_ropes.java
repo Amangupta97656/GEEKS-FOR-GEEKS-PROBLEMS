89fd2ade5108a7953d2d20fd8bda92fbae816fa0
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-10-26 21:01:45 +0530

    Add files via upload

--- Minimum_cost_of_ropes.java
+++ Minimum_cost_of_ropes.java
@@ -1,24 +1,18 @@
 class Solution {
-    // Function to return the minimum cost of connecting the ropes.
-    public long minCost(long[] arr) {
-        PriorityQueue<Long> pq = new PriorityQueue<>();
-        ArrayList<Long>ls = new ArrayList<>();
-        for(long a : arr){
-            pq.add(a);
-        }
-        
-        while(pq.size()>1){
-            long n = pq.poll() + pq.poll();
-            
-            ls.add(n);
-            pq.add(n);
-        }
-        
-        long ans = 0;
-        
-        for(int i = 0; i<ls.size(); i++){
-            ans += ls.get(i);
-        }
-        return ans;
+    public static int minCost(int[] arr) {
+       if(arr.length==1) return 0;
+       
+       PriorityQueue<Integer> pq=new PriorityQueue<>();
+       int res=0;
+       for(int i : arr) pq.offer(i);
+       
+       while(pq.size()>=2){
+           int a=pq.poll(),b=pq.poll();
+          
+           res+=a+b;
+           pq.offer(a+b);
+       }
+       
+       return res;
     }
 }
