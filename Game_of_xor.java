f8c6837925ec022d0df6a0a510b5b308f02bb0f6
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-11-25 20:44:13 +0530

    Add files via upload

---
+++ Game_of_xor.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int subarrayXor(int[] arr) {
+          int ans=0;
+        int co=1;
+        if(arr.length %2 ==0) return 0;
+        
+        for(int i: arr){
+            if(co%2!=0){
+                ans=ans^i;
+            }
+            co++;
+        }
+        return ans;
+    }
+}
+
