2f15bf42cf296c2f1df2e11ba477bbb8fe01ce35
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-12-07 21:25:54 +0530

    Add files via upload

--- Number_of_distinct_subsequences.java
+++ Number_of_distinct_subsequences.java
@@ -1,20 +1,21 @@
 class Solution {
-    int distinctSubsequences(String S) {
-        int mo=1000000007;
-        int n=S.length();
-        int dp[]=new int[n+1];
-        dp[0]=1;
-        HashMap<Character ,Integer> map=new HashMap<>();
-        for(int i=1;i<=n;i++){
-            
-            dp[i]=(2*dp[i-1])%mo;
-            char ch=S.charAt(i-1);
-            if(map.containsKey(ch)){
-                int j=map.get(ch);
-                dp[i]=(dp[i]-dp[j-1]+mo)%mo;
-            }
-            map.put(ch,i);
+    int distinctSubseq(String str) {
+        int mod = 1000000007;
+        int n = str.length();
+        long[] dp = new long[n + 1];
+        int[] last = new int[26];
+
+        for (int i = 0; i < 26; i++) last[i] = -1;
+
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = (dp[i - 1] * 2) % mod;
+            int c = str.charAt(i - 1) - 'a';
+            if (last[c] != -1) dp[i] = (dp[i] - dp[last[c] - 1] + mod) % mod;
+            last[c] = i;
         }
-        return dp[n];
+
+        return (int) dp[n];
     }
 }
