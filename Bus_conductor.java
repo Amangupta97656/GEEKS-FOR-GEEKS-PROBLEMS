52515504891d93352862efa87b70ea6c8d2b4b9e
AMAN GUPTA <113255969+Amangupta97656@users.noreply.github.com>
2025-12-19 16:21:24 +0530

    Add files via upload

--- Bus_conductor.java
+++ Bus_conductor.java
@@ -1,10 +1,13 @@
 class Solution {
-    public static int findMoves(int n, int[] chairs, int[] passengers) {
-        Arrays.sort(chairs);
-        Arrays.sort(passengers);
-        int ans=0;
-        for(int i=0;i<n;i++)
-            ans+=Math.abs(chairs[i]-passengers[i]);
-        return ans;
+    public int findMoves(int[] chairs, int[] passengers) {
         // code here
-  
+         Arrays.sort(chairs);
+        Arrays.sort(passengers);
+        int result=0;
+        for(int i=0;i<chairs.length;i++){
+            result+=Math.abs(chairs[i]-passengers[i]);
+        }
+        return result;
+        
+    }
+}
